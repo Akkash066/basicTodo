ace6437c92542dacbe775035b6eba4dd49cbc427
Akash <akashk494233@gmail.com>
2025-03-28 16:03:15 +0530

    modify and adding some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import TodoList from './TodoList'
 import LotteryTicket from './LotteryTicket'
 import React from 'react'
-
+// import Ticket from './Ticket'
 
 const App = () => {
   return (
     <div>
       {/* <TodoList /> */}
-      <LotteryTicket />
+      {/* <LotteryTicket /> */}
+      {/* <Ticket ticket={[0, 1, 3]}/>
+      <Ticket ticket={[0, 1, 3, 6, 4]}/> */}
+      <LotteryTicket n={3} winnigSum={15}/>
     </div>
   )
 }
